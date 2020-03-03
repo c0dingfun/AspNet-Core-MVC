a1d382ed87f5d94f798ef9a3b9ed8b5a0e3e56ff
c0dingfun <hackerrank888@gmail.com>
2020-03-03 09:46:24 -0800

    Modified DB Connection to use AuthenticationDB. Did Database Migration

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -29,7 +29,7 @@ namespace WebUI
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("AuthenticationDBConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
